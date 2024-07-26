c899844c119f68650ae8f543ec9897227ce41817
Lucas Scotti <lucas.2023014216@alunocesul.com>
2024-07-25 22:39:15 -0300

    Add files via upload

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,33 @@
+﻿using System; 
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.Nome = "Lucas";
+            pessoa.Idade = 22;
+
+            pessoa.Apresentar();
+
+            Console.WriteLine("Olá Mundo C#");
+        }
+    }
+
+    class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome} e eu tenho {Idade} anos.");
+        }
+    }
+
+    
+
+    
+}
